6032596d0b664d80daf73c8d48189256d4dc0452
Kyungwan Han <asura321@hanmail.net>
2017-02-20 05:07:29 +0900

    Update acmicpc-2750-수 정렬하기.c

--- acmicpc-2750-수 정렬하기.c
+++ acmicpc-2750-수 정렬하기.c
@@ -28,33 +28,8 @@ void sort()
 		printf("%d\n", Input[i]);
 }
 
-void sort2()
-{
-	scanf("%d\n", &N);
-	for (int i = 0; i <= N_MAX + 1; i++)
-		cnt[i] = 0;
-
-	for (int i = 0; i < N; i++)
-	{
-		int input;
-		scanf("%d", &input);
-		cnt[input]++;
-	}
-
-	for (int i = 1; i < N_MAX; i++)
-	{
-		if (cnt[i] == 0)
-			continue;
-		int c = cnt[i];
-		for (int j = 0; j < c; j++)
-			printf("%d\n", i);
-	}
-}
 int main(int argc, const char * argv[]) {
 
 	sort();
-//	sort2();
-
-
 	return 0;
 }
